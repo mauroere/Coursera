43686ff47254ca0371b8e53117ac60faaf28ee24
Damian Hintz <damian.hintz@gmail.com>
2016-11-28 20:08:03 +0100

    Compute cost fuction

--- MachineLearning/Assignments/ex1/computeCost.m
+++ MachineLearning/Assignments/ex1/computeCost.m
@@ -6,7 +6,7 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
@@ -14,8 +14,9 @@ J = 0;
 %               You should set J to the cost.
 
 
-
-
+predictions = X * theta;
+errors = (predictions-y).^2;
+J = sum(errors)/(2*m);
 
 % =========================================================================
 
